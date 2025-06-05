c33b4212373513b390e2fe8a34432e9d0fb4cbc4
Pranay-Gottimukula <pranaygottimukula@gmail.com>
2025-06-05 20:43:01 +0530

    Add commentary text generation

--- scripts/generate_commentary.py
+++ scripts/generate_commentary.py
@@ -1,6 +1,12 @@
 from dotenv import load_dotenv
 import os
 from groq import Groq
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from prompts.prompt_generator import generate_prompt
+
 
 '''
     This code generates text
@@ -16,12 +22,27 @@ client = Groq(
     api_key = os.getenv("GROQ_API_KEY"),
 )
 
+prompt: str = generate_prompt(commentator="Ravi Shastri", event="six", start_time=2.3,
+                            end_time=16.6, tts_speed=2.5, 
+                            player_names = {
+                                "batsman" : "Virat",
+                                "bowler" : "Dhoni"
+
+                            },
+                            context = {
+                                "batting_team" : "Royal Challengers Bangalore",
+                                "bowling_team" : "Chennai super kings",
+                                "over" : 17.2,
+                                "req_runrate" : 8.5,
+                                "current_score" : "142/4"
+                            })
+
 # Collecting response (testing)
 chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Explain the importance of fast language models",
+            "content": prompt,
         }
     ],
     model="llama-3.3-70b-versatile",
--- scripts/pipeline_runner.py
+++ scripts/pipeline_runner.py
@@ -1,5 +1,5 @@
 # Pipeline which handles input video
 
-def run_pipeline(input_path, output_path):
+def run_pipeline(input_path: str, output_path: str):
     print("Input path: " + input_path)
     print("Output path: " + output_path)
