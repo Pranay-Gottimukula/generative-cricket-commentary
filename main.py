e0c4cb122632f5b722c31a39335e6ad66a8c5175
Pranay Gottimukula <pranaygottimukula@gmail.com>
2025-06-06 19:50:20 +0530

    minor fixes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from scripts.pipeline_runner import run_pipeline
     Collects Input path and sends it through pipeline
 '''
 
+
 if __name__ == "__main__":
     args = run_cli()
     run_pipeline(input_path=args.input, output_path=args.output)
